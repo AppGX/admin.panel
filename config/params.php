6ae71e40dacd07acab29f069d6c702531ca7701b
AppGX <apocalypsis@mail.ru>
2020-12-23 11:02:53 +0600

    create pattern

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+    return [
+        'admin_email' => 'admin@admin.com',
+        'shop_name' => 'Laravel Shop',
+        'pagination' => 3,
+        'smtp_host' => 'smtp.mail.ru',
+        'smtp_port' => '2525',
+        'smtp_protocol' => 'ssl',
+        'smtp_login' => 'app@mail.ru',
+        'smtp_pass' => 'jk3hd2fjs32423',
+        'login' => 'admin.panel',
+    ];
